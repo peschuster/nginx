bedc8f43b32f32fa3278d0bae9621d8166ff2a14
Peter Schuster <schuster.pe@gmail.com>
2012-12-22 12:56:03 +0100

    further fixes

--- src/os/unix/ngx_process_memguard.c
+++ src/os/unix/ngx_process_memguard.c
@@ -22,19 +22,19 @@ char* _ngx_process_memguard_find_line(char *line)
     return (strncmp(p, match,  match_len) == 0) ? (p + match_len) : NULL;
 }
 
-char ngx_master_process_memguard_triggered(ngx_atomic_uint_t min_mem)
+char ngx_master_process_memguard_triggered(long min_mem)
 {
     char *p, *pend;
     char line[MAXLINE];
-    ngx_atomic_uint_t fmem = 0;
+    long fmem = 0;
     FILE *proc_meminfo = fopen("/proc/meminfo", "r");
 
     if (!proc_meminfo) {
         return 0;
 	}
 	
-	while (p = fgets(line, MAXLINE, proc_meminfo)) {        
-		if (p = _ngx_process_memguard_find_line(line)) {
+	while ((p = fgets(line, MAXLINE, proc_meminfo))) {
+		if ((p = _ngx_process_memguard_find_line(line))) {
 			
 			 // check last char for newline terminator
             pend = p + strlen(p) - 1;
